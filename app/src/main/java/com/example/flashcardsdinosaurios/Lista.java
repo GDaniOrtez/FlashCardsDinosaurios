a8ccee453a72dca94ad4264a5f1f8851deb78e6b
Genesis Ortez <gdaniortez@gmail.com>
2019-07-06 19:49:05 -0600

    Primer Comit

---
+++ app/src/main/java/com/example/flashcardsdinosaurios/Lista.java
@@ -0,0 +1,15 @@
+package com.example.flashcardsdinosaurios;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Lista extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista);
+    }
+    
+}
